3608a4b7785dc3aaafe5c3738976c86d695c9a4b
Emina <you@example.com>
2022-10-03 12:50:55 +0200

    English corrections

--- src/constants/locales/en.ts
+++ src/constants/locales/en.ts
@@ -20,44 +20,52 @@ const en = {
     },
     about: {
         title: 'About us',
-        p1: `Our apartments are located directly on the <span class="has-text-weight-bold">banks of the Drina river</span>, between stunning mountain hills. We invested time in understanding the demands of the soul for <span class="has-text-weight-bold">rest and relaxation</span>, while touring the world for many years, and came up with the idea of apartments on the Drina. River Drina Apartments & Wellness attempts to provide a distinctive experience by fusing modern style and genuine ambience, far from the city's hustle and bustle and left to the charms of untouched nature.`,
-        p2: `We hoped that by completing this project and realizing our dream, we would be able to introduce visitors to our <span class="has-text-weight-bold">beautiful homeland and its natural beauty</span>. The complex has three luxuriously equipped apartments with a capacity of 10 to 12 guests, as well as a wellness center with a relaxing sauna and a salt water indoor pool.`,
-        p3: `There is a designated area within the fenced yard for children's entertainment, as well as direct access to the lovely banks of the Drina. Additionally, the romantic summer house provides a lovely view of the Drina river and a fireplace and BBQ tools for a completely hedonistic experience and enjoyment of the nights by this tempting river.Within the property, there is free parking and a cutting-edge surveillance system.`,
-        p4: ``,
-        p5: ``,
+        p1: `River Drina Apartments &amp; Wellness, located in <span class="has-text-weight-bold">untouched nature,</span>, offers a unique experience by combining <span class="has-text-weight-bold">modern style</span> and <span class="has-text-weight-bold">authentic ambiance,.</span>, far away from the hustle and bustle of the city.`,
+        p2: `The concept of <span class="has-text-weight-bold">River Drina Apartments &amp; Wellness</span> on beautiful rivers banks, is developed through worldwide travel experiences and studying the needs of <span class="has-text-weight-bold">body</span> and <span class="has-text-weight-bold">soul</span>, as well as the desire to invite you to explore our beautiful homeland and untouched nature.`,
+        p3: `The complex has three <span class="has-text-weight-bold">luxuriously</span> equipped apartments with a capacity of 12 guests and a wellness center including a comfortable <span class="has-text-weight-bold">sauna</span> and an <span class="has-text-weight-bold">indoor saltwater pool.</span>`,
+        p4: `There is a spacious <span class="has-text-weight-bold">children’s playground</span> within the fenced area, while our <span class="has-text-weight-bold">romantic pergola</span> offers a beautiful view of the Drina River and includes a fireplace and barbecue for a completely <span class="has-text-weight-bold">hedonistički doživljaj.</span>`,
+        p5: `<span class="has-text-weight-bold">Free parking</span> is available on site, as well as a modern <span class="has-text-weight-bold">surveillance system.</span>`,
     },
     apartments: {
         apartment: 'apartment',
         capacityInfo: 'Every unit has the option of increasing the number of beds.',
         bookNow: 'book now',
         apartment1: {
-            description: `This <span class="has-text-weight-bold">luxury apartment</span> has a <span class="has-text-weight-bold">master bedroom</span>, a large <span class="has-text-weight-bold">living area</span>, a <span class="has-text-weight-bold">kitchen</span>, and a <span class="has-text-weight-bold">bathroom</span>. The view from the apartment is of <span class="has-text-weight-bold">stunning, lush hills</span>. This apartment can accommodate up to 4 people. It is appropriate for everyone who values <span class="has-text-weight-bold">peace and space</span>. The complex includes a <span class="has-text-weight-bold">Finnish sauna</span> that will calm your senses and an <span class="has-text-weight-bold">indoor saltwater pool</span>.`
+            description: `This <span class="has-text-weight-bold">luxury apartment</span> includes a <span class="has-text-weight-bold">master bedroom</span>, a large <span class="has-text-weight-bold">living area</span>, a <span class="has-text-weight-bold">kitchen</span>, and a <span class="has-text-weight-bold">bathroom</span>. The view from the apartment is of <span class="has-text-weight-bold">stunning, lush hills</span>. 
+ <span class="is-block p-t-15 p-b-15">
+It is highly recommended for everyone who values <span class="has-text-weight-bold">peace and space</span>. The complex includes a <span class="has-text-weight-bold">Finnish sauna</span> that will calm your senses and an <span class="has-text-weight-bold">indoor saltwater pool</span>. </span>`
         },
         apartment2: {
-            description: `You may enjoy the <span class="has-text-weight-bold">beautiful view</span> of the Drina from the <span class="has-text-weight-bold">balcony</span> of the luxury apartment. One <span class="has-text-weight-bold">bedroom</span>, a <span class="has-text-weight-bold">living room</span> with a <span class="has-text-weight-bold">kitchen</span>, and a bathroom are all included in the apartment. We equipped each apartment with all the necessities for optimum <span class="has-text-weight-bold">relaxation and enjoyment</span> in the kitchen. The apartment may accommodate up to four guests. A <span class="has-text-weight-bold">saltwater indoor pool</span> and a <span class="has-text-weight-bold">Finnish sauna</span> are also located within the complex, for relaxation purposes.`
+            description: `You may enjoy the <span class="has-text-weight-bold">beautiful view</span> of the Drina from the <span class="has-text-weight-bold">balcony</span> of the luxury apartment. One <span class="has-text-weight-bold">bedroom</span>, a <span class="has-text-weight-bold">living room</span> with a <span class="has-text-weight-bold">kitchen</span>, and a <span class="has-text-weight-bold">bathroom</span> are all included in the apartment. We equipped each apartment with all the necessities for optimum <span class="has-text-weight-bold">relaxation</span> and <span class="has-text-weight-bold">cooking.</span>
+   <span class="is-block p-t-15 p-b-15">
+  A <span class="has-text-weight-bold">saltwater indoor pool</span> and a <span class="has-text-weight-bold">Finnish sauna</span> are also located within the complex for relaxation purposes.</span>`
         },
         apartment3: {
-            description: `A studio apartment at River Drina Apartments & Wellness is ideal for two people who want to take advantage of the <span class="has-text-weight-bold">peace and intimacy</span> the property has to offer. The apartment provides all required equipment and access to a<span class="has-text-weight-bold"> beautiful balcony</span> with a view of the Drina river and forested hills. A <span class="has-text-weight-bold">saltwater indoor pool</span> and a <span class="has-text-weight-bold">Finnish sauna</span> are also part of the complex, both of which will fully soothe you.`
+            description: `A studio apartment at River Drina Apartments & Wellness is ideal for guests who want to take advantage of the <span class="has-text-weight-bold">peace and intimacy</span> the property has to offer. 
+   <span class="is-block p-t-15 p-b-15">
+The apartment is <span class="has-text-weight-bold">fully equipped</span> and includes a <span class="has-text-weight-bold"> beautiful balcony</span> with a view of the Drina river and forested hills. </span>
+A <span class="has-text-weight-bold">saltwater indoor pool</span> and a <span class="has-text-weight-bold">Finnish sauna</span> are also part of the complex, both of which will fully soothe you.`
         },
     },
     quote: `<span class="has-text-weight-bold">River Drina</span> Apartments & Wellness welcomes you to a <span class="has-text-weight-bold">magical vacation</span>`,
     wellness: {
         title: 'Wellness & spa',
-        text: `We offer a wellness center with an indoor pool and salt water that is rich in therapeutic characteristics to give you a totally relaxing experience. `,
+        text: `There is a wellness area with a Finnish sauna and an indoor saltwater pool that is rich in therapeutic properties, offering you a luxury relaxation experience. `,
         sauna: `Sauna`,
-        saunaText: `A Finnish sauna, which is proven to improve health and well-being, is also part of the wellness area. The skin, face, and entire body look better after spending time in the sauna. Muscles unwind, and a person's general psychological state gets better when they use a sauna. Regular use strengthens the body's defense mechanisms, burns calories, reduces stress, and allows pores to naturally open and perspiration naturally expels dust, dead skin, and pollutants. It is not surprising that one of our visitors' favorite spots is the sauna in our wellness area, because it was constructed with the best materials and can accommodate four people.`,
+        saunaText: `The wellness area with a Finnish sauna, in addition to relaxing, also has a positive impact on health. A stay in the sauna results in beautiful appearance of the skin and the whole body. Regular use improves the body&#39;s resistance, burns calories, relieves stress, and removes toxins from the body. We built the sauna from high quality materials and it offers relaxation for 4 people, so it is not surprising that it is one of the favorite corners of our guests.`,
     },
     pool: {
         title: `Swimming Pool`,
-        text: `The pool provides a singular opportunity to enjoy the view of the Drina in any season due to its exceptional placement. The pool is <span class="has-text-weight-bold">11 m x 4.5 m</span> in size, with a progressive depth increase from <span class="has-text-weight-bold">1.35 m to 1.95 m</span> and a constant temperature of <span class="has-text-weight-bold">29 °C</span>, which makes swimming completely enjoyable. The hydro-aero massage and waterfall effects also help to completely relax and massage the body. The pool is equipped with sophisticated lighting effects and sunbeds for rest and relaxation to complete the atmosphere.
-        <br/><span class="has-text-weight-bold">The pool contains salt water</span> without chlorine thanks to the most advanced technology used in pool maintenance, which makes it safe for everyone who is allergic to chlorine, such as children, elderly, people with sensitive eyes, and people with sinus and respiratory issues. The benefits of this water are also numerous for regular swimmers.`,
+        text: `You have the possibility of enjoying the view of the Drina in all seasons, from the indoor saltwater pool. The dimensions of the pool (<span class="has-text-weight-bold">11 m x 4.5 m</span>) with a gradual increase in depth from <span class="has-text-weight-bold">1.35 m to 1.95 m</span> and a constant water temperature of <span class="has-text-weight-bold">29 °C</span>, offers pleasure in swimming throughout the year. The hydro-aero massage and waterfall contribute to full relaxation of the
+body. The pool is equipped with modern lighting and sound effects, as well as sunbeds for rest and relaxation.
+        <span class="is-block p-t-15 p-b-15">Thanks to latest technologies and investment in the wellness area, the pool contains <span class="has-text-weight-bold">chlorine-free saltwater</span>. Which is suitable for all those who are hypersensitive to chlorine, such as children, elderly, as well as those with sensitive eyes and sinuses, and the advantages of this water are for the swimmers themselves, multiple.        </span>`,
     },
     location: {
         title: `Location`,
-        text: `River Drina Apartments & Wellness are located on the riverbank of the Drina River, near Bratunac. Drina's longest part flows directly through the town of Bratunac and contributes to the area with true beauty as it is surrounded by forested hills. 
-        <span class="is-block">Because of their position in Bratunac, our apartments are ideal for both a restful and an active vacation with sports like hiking, cycling, fishing, and discovering the Drina's gorgeous banks.</span>
-        Drina river offers boating opportunities thanks to its abundance, and this calm section of the river also allows for swimming.
-        <span class="is-block">For those who prefer an active vacation and wish to explore their surroundings, the proximity of significant cultural attractions will be a plus.</span>`,
+        text: `River Drina Apartments &amp; Wellness is located on the shores of the Drina River and in the immediate surroundings of Bratunac. The longest part of the Drina runs through the town of Bratunac and contributes to the exceptional beauty of this area surrounded by forested green hills. 
+        <span class="is-block">Thanks to its location, River Drina Apartments &amp; Wellness are suitable for a relaxing holiday, but also for activities such as hiking, cycling, fishing, and exploring the beauty of the banks of the Drina.</span>
+With its wealth, the Drina offers the possibility of boating but also swimming in this part of the same river.
+        <span class="is-block">The proximity of important cultural destinations will satisfy those who enjoy an active holiday and exploring surroundings where they spend their vacation.</span>`,
     },
     welcome: {
       text: `Welcome to your perfect vacation!`,
